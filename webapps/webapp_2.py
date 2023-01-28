de2f331f7b0f958029b0aff00e63a171f48d72d1
johannaabheiden <116267442+johannaabheiden@users.noreply.github.com>
2023-01-28 18:58:23 +0100

    Update webapp_2.py
    ggg

--- webapps/webapp_2.py
+++ webapps/webapp_2.py
@@ -5,23 +5,21 @@ from pyodide.ffi import create_proxy, to_js
 # Import python module
 import math
 
-# Version mit gui slider at CURRENT_ITERATIONS
+global geom_params   
 
-geom1_params=  {
-        "x": 5,
-        "angle1":1.5,
-        "angle2":3,
-        "translation_z":0,
-        "moving_z": -2,
-        "amountz": 2,
-        "amountx": 4      
+geom_params = {
+    "x": 1,
+    "division":0.5
 }
 
-geom1_params = Object.fromEntries(to_js(geom1_params))
+geom_params = Object.fromEntries(to_js(geom_params))
 
-#-----------------------------------------------------------------------
-# USE THIS FUNCTION TO WRITE THE MAIN PROGRAM
+# meherere Copien dazumachen
+# bessere Auflösung
+
+
 def main():
+
     #-----------------------------------------------------------------------
     # VISUAL SETUP
     # Declare the variables
@@ -34,180 +32,362 @@ def main():
     document.body.appendChild(renderer.domElement)
 
     # Set up the scene
-
-    
     scene = THREE.Scene.new()
-    back_color = THREE.Color.new(0.1,0.1,0.1)
+    back_color = THREE.Color.new()
     scene.background = back_color
-    camera = THREE.PerspectiveCamera.new(1000, window.innerWidth/window.innerHeight, 0.6, 1000)
-    camera.position.z = 10
-    camera.position.y = -30
-    camera.position.x = 30
+    camera = THREE.PerspectiveCamera.new(50, window.innerWidth/window.innerHeight, 0.6, 50)
+    camera.position.z = 0
+    camera.position.y = 1
+    camera.position.x = -4
+
+    # Set up lighting
+    amb_light = THREE.AmbientLight.new(0xf0f0f0)
+    scene.add(amb_light)
+    light = THREE.SpotLight.new(0xffffff, 1.5 )
+    light.position.set( 0, 1500, 200 )
+    light.angle = math.pi * 0.3
+    light.castShadow = True
+    light.shadow.camera.near = 200
+    light.shadow.camera.far = 2000
+    light.shadow.bias = - 0.000222
+    light.shadow.mapSize.width = 1024
+    light.shadow.mapSize.height = 1024
+    scene.add( light )
+
     scene.add(camera)
 
-    #colors
-
-    global color1, color2
-
-    color1 =  THREE.Color.new('rgb(600, 127, 64)')
-    color2 = THREE.Color.new('rgb(0,0,255)')
-
-    # defintion materials
-
-    global line_material1, line_material2 
-
-    line_material1 = THREE.LineBasicMaterial.new()
-    line_material1.color = color2
-    line_material1.transparent = True
-    line_material1.opacity = 0.6
+    global line_material2, meshmaterial
 
     line_material2 = THREE.LineBasicMaterial.new()
-    line_material2.color = color1
-    line_material2.transparent = True
-    line_material2.opacity = 0.6
+    line_material2.color = THREE.Color.new('rgb(0, 0, 128)')
 
-    
+    meshmaterial = THREE.MeshBasicMaterial.new()
+    #meshmaterial.color = THREE.Color.new('rgb(240, 248, 255)')
+    meshmaterial.transparent = True
+    meshmaterial.opacity = 0.8
+    meshmaterial.side = 2
 
-
-    # Graphic Post Processing
     global composer
     post_process()
 
     # Set up responsive window
     resize_proxy = create_proxy(on_window_resize)
-    window.addEventListener('resize', resize_proxy) 
-    #-----------------------------------------------------------------------
-    
-    design()
+    window.addEventListener('resize', resize_proxy)
 
-    #update_draw_system((my_axiom_system), THREE.Vector3.new(0,0,0))
-
-    #-----------------------------------------------------------------------
-    # USER INTERFACE
+    # programming user interface----------------------------------------------------------------------------------------------
     # Set up Mouse orbit control
     controls = THREE.OrbitControls.new(camera, renderer.domElement)
 
+
     # Set up GUI
-    gui = window.dat.GUI.new()
-    param_folder = gui.addFolder('Parameters')
-    param_folder.add(geom1_params, 'x', 0,5,1)
-    param_folder.add(geom1_params,'angle1', 1,2,0.1)
-    param_folder.add(geom1_params, 'angle2', 2,3,0.1)
-    param_folder.add(geom1_params, 'translation_z',0,0.06,0.01)
-    param_folder.add(geom1_params, 'moving_z', -20, 0, 1)
-    param_folder.add(geom1_params, 'amountz', 0, 10, 1)
+    gui = window.lil.GUI.new()
+    gui.add( geom_params, 'x', 0,5,1)
+    gui.add( geom_params, 'division', 0,1,0.1)
+
+    #gui.add( params, 'tension',0,1).step( 0.01 ).onChange(on_changed(params.tension))
     
+    gui.open()
+    
+
+    global max_iterations, division
+    max_iterations= geom_params.x
+    division = geom_params.division
+
    
-    param_folder.open()
+    #defintion of the system
+    my_element_system = system(0,max_iterations,"X")
+    draw_system(my_element_system)
     
-    #-----------------------------------------------------------------------
-    # RENDER + UPDATE THE SCENE AND GEOMETRIES
     render()
-    
-#-----------------------------------------------------------------------
 
-def design():
+def system(current_iteration, max_iterations ,element):
+    current_iteration += 1
+    new_element = ""
+    for symbol in element:
+        new_element += generate(symbol)
+    if current_iteration > max_iterations:
+        return new_element
+    else:
+        return system(current_iteration, max_iterations, new_element)
 
-    # YOUR DESIGN / GEOMETRY GENERATION
-    # Geometry Creation
-    global max_iterations, moving_z, amountz
-    max_iterations = geom1_params.x
-    moving_z = geom1_params.moving_z
-    amountz = geom1_params.amountz
-   
-
-    my_molecule_system = system(0, max_iterations, "X")   
-    for i in range(amountz):
-            draw_system((my_molecule_system), THREE.Vector3.new(0,moving_z*i,0))
-           
-
-# HELPER FUNCTIONS
-# Define RULES in a function which takes one SYMBOL and applies rules generation
 def generate(symbol):
     if symbol == "X":
-        return "X+X-XX"
-
-    elif symbol == "+":
-        return "+"
-    elif symbol == "-":
-        return "-"
-
-# A recursive fundtion, which taken an AXIOM as an inout and runs the generate function for each symbol
-
-def draw_system(molecule, start_pt):
-
-    global lines, angle1, angle2, translationvector
-    translationvector = geom1_params.translation_z
-    move_vec = THREE.Vector3.new(1,0,0)
-    lines = []
-
-    for symbol in molecule:
-
-        if symbol == "X":
-            old = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
-            new_pt = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
-            new_pt = new_pt.add(move_vec)
-            line = []
-            line.append(old)
-            line.append(new_pt)
-            lines.append(line)
-            start_pt = new_pt
-
-        elif symbol == "+":
-            angle1 = geom1_params.angle1
-
-            move_vec.applyAxisAngle(THREE.Vector3.new(0,-1,translationvector), math.pi/-(angle1))
-        
-        elif symbol == "-":
-
-            angle2 = geom1_params.angle2
-            move_vec.applyAxisAngle(THREE.Vector3.new(0,-1,0), math.pi/(angle2))
-
-    for points in lines:
-
-        global geom, scene
-
-        line_geom = THREE.BufferGeometry.new()
-        points = to_js(points)
-        geom = THREE.SphereGeometry.new(0.2,10, 10)
-        geom.translate(points[0].x, points[0].y, points[0].z)
-        sphere = THREE.Mesh.new(geom)
-        
-        
-        edges = THREE.EdgesGeometry.new(sphere.geometry)
-        edgeline = THREE.LineSegments.new(edges, line_material2)
-        scene.add(edgeline)
-
-        line_geom.setFromPoints( points ) 
-        vis_line = THREE.Line.new( line_geom, line_material1 )       
-        scene.add(vis_line)
-
-        
-def system(current_iteration, max_iterations, molecule):
-    current_iteration += 1
-    new_molecule = ""
-    for symbol in molecule:
-        new_molecule += generate(symbol)
-    if current_iteration >= max_iterations:
-        return new_molecule
-    else:
-        return system(current_iteration, max_iterations, new_molecule)
+        return "F[X]"
+    elif symbol == "F":
+        return "FF"
+    elif symbol == "[":
+        return "["
+    elif symbol == "]":
+        return "]"
 
 
-def update_system():
-    global max_iterations, moving_z, amountz, scene
-
-    if max_iterations != geom1_params.x or angle1 != geom1_params.angle1 or angle2 != geom1_params.angle2 or translationvector != geom1_params.translation_z or moving_z != geom1_params.moving_z or geom1_params.amountz != amountz:
-        scene.clear()
-        design()
+def draw_system(element):
+    #creating basic start_points
     
+    geometries = []
+    
+    start_pt = THREE.Vector3.new(0,0,0)
+    move_vec2 = THREE.Vector3.new(0,0,1)
+    move_vec3 = THREE.Vector3.new((3**(1/2))/2,0,0.5)
+    move_vec4 = THREE.Vector3.new((1/3)*(3**(1/2))/2, 1*((2/3)**(1/2)),0.5)
+
+    start_pt2 = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+    start_pt2.add(move_vec2)
+    start_pt3 = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+    start_pt3.add(move_vec3)
+    start_pt4 = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+    start_pt4.add(move_vec4)
+
+    start_points = []
+    start_points.append(start_pt)
+    start_points.append(start_pt2)
+    start_points.append(start_pt3)
+    start_points.append(start_pt4)
+
+    triangular1 = [start_points[0], start_points[1], start_points[3], start_points[0]]
+    triangular2 = [start_points[1], start_points[2], start_points[3], start_points[1]]
+    triangular3 = [start_points[2], start_points[0], start_points[3], start_points[3]]
+
+    triangular1 = to_js( triangular1 )
+    triangular2 = to_js( triangular2)
+    triangular3 = to_js( triangular3)
+    
+    TRIANUGLAR1 = THREE.BufferGeometry.new().setFromPoints(triangular1)
+    TRIANULAR1_MESH = THREE.Line.new(TRIANUGLAR1, line_material2)
+    #TRIANGULAR1_SURFACE = THREE.Mesh.new(TRIANUGLAR1, meshmaterial)
+    scene.add( TRIANULAR1_MESH)
+    #scene.add(TRIANGULAR1_SURFACE)
+    geometries.append(TRIANULAR1_MESH)
+
+    TRIANUGLAR2 = TRIANUGLAR1.clone().setFromPoints(triangular2)
+    TRIANUGLAR2_MESH = THREE.Line.new(TRIANUGLAR2, line_material2)
+    #TRIANGULAR2_SURFACE = THREE.Mesh.new(TRIANUGLAR2, meshmaterial)
+    scene.add( TRIANUGLAR2_MESH )
+    #scene.add(TRIANGULAR2_SURFACE)
+    geometries.append(TRIANUGLAR2_MESH)
+
+    TRIANUGLAR3 = TRIANUGLAR1.clone().setFromPoints(triangular3)
+    TRIANUGLAR3_MESH = THREE.Line.new(TRIANUGLAR3, line_material2)
+    #TRIANGULAR3_SURFACE = THREE.Mesh.new(TRIANUGLAR3, meshmaterial)
+
+    scene.add( TRIANUGLAR3_MESH )
+    #scene.add(TRIANGULAR3_SURFACE)
+    geometries.append(TRIANUGLAR3_MESH)
+
+    #basic triangles FACE1
+
+    mid_points=[]
+
+    line0_1= THREE.LineCurve.new(start_points[0],start_points[1])
+    midpoint0_1 = THREE.Vector3.new()
+    line0_1.getPoint(0.5, midpoint0_1)
+    line0_3= THREE.LineCurve.new(start_points[0],start_points[3])
+    midpoint0_3 = THREE.Vector3.new()
+    line0_3.getPoint(0.5, midpoint0_3)
+    line1_2= THREE.LineCurve.new(start_points[1],start_points[2])
+    midpoint1_2= THREE.Vector3.new()
+    line1_2.getPoint(0.5, midpoint1_2)
+    line1_3= THREE.LineCurve.new(start_points[1],start_points[3])
+    midpoint1_3 = THREE.Vector3.new()
+    line1_3.getPoint(0.5, midpoint1_3)
+    line2_0= THREE.LineCurve.new(start_points[2],start_points[0])
+    midpoint2_0= THREE.Vector3.new()
+    line2_0.getPoint(0.5, midpoint2_0)
+    line2_4= THREE.LineCurve.new(start_points[2],start_points[3])
+    midpoint2_4 = THREE.Vector3.new()
+    line2_4.getPoint(0.5, midpoint2_4)
+
+    mid_points.append(midpoint0_1)   
+    mid_points.append(midpoint0_3)   
+    mid_points.append(midpoint1_2)   
+    mid_points.append(midpoint1_3)   
+    mid_points.append(midpoint2_0)   
+    mid_points.append(midpoint2_4) 
+
+
+    FACE1 = []
+    FACE1.append(start_points[0])
+    FACE1.append(start_points[1])
+    FACE1.append(start_points[3])
+    FACE1.append(mid_points[0])
+    FACE1.append(mid_points[3])
+    FACE1.append(mid_points[1])
+
+
+    face1_middle =[FACE1[3], FACE1[4], FACE1[5], FACE1[3]]
+    face1_middle = to_js(face1_middle)
+    FACE1_mid = THREE.BufferGeometry.new().setFromPoints(face1_middle)
+
+    FACE1_mid_mesh= THREE.Line.new(FACE1_mid, line_material2)
+    scene.add(FACE1_mid_mesh)
+    
+    geometries.append(FACE1_mid_mesh)
+
+    face1_1_start1 = start_points[0]
+    face1_1_start2 = midpoint0_1
+    face1_1_start3 = midpoint0_3
+    
+    face2_1_start1 =  midpoint0_1 
+    face2_2_start2 = midpoint1_3
+    face2_3_start3 = midpoint0_3
+    
+    face3_1_start1 = midpoint1_3
+    face3_2_start2 = start_points[3]
+    face3_3_start3 =  midpoint0_3
+
+
+    face4_1_start1 = midpoint0_1
+    face4_2_start2 = start_points[1]
+    face4_3_start3 =  midpoint1_3
+    
+    for symbol in element:
+    
+       
+        if symbol == "F" or symbol == "X":
+
+            
+
+            face1_1line_points = [face1_1_start1, face1_1_start2, face1_1_start3, face1_1_start1]
+            face1_1line_points = to_js(face1_1line_points)
+
+            face1_1line_geom = THREE.BufferGeometry.new()
+            face1_1line_geom.setFromPoints(face1_1line_points)
+            face1_1line_mesh = THREE.Line.new( face1_1line_geom, line_material2)
+            scene.add(face1_1line_mesh)
+
+            geometries.append( face1_1line_mesh)
+
+            face1_1_1 = THREE.LineCurve.new(face1_1_start1, face1_1_start2 )
+            face1_1mid = THREE.Vector3.new()
+            face1_1_1.getPoint(division, face1_1mid)
+            face1_1_2 = THREE.LineCurve.new(face1_1_start2,face1_1_start3 )
+            face1_1_2mid = THREE.Vector3.new()
+            face1_1_2.getPoint(division,face1_1_2mid)
+            face1_1_3 = THREE.LineCurve.new(face1_1_start3,face1_1_start1 )
+            face1_1_3mid = THREE.Vector3.new()
+            face1_1_3.getPoint(division, face1_1_3mid)
+            
+            face1_1_start1 = face1_1mid
+            face1_1_start2 = face1_1_2mid
+            face1_1_start3 = face1_1_3mid
+
+            #face2---------------------------------------------------------------------------------------------------------------------------
+            
+            face2_1line_points = [face2_1_start1, face2_2_start2, face2_3_start3, face2_1_start1]
+            face2_1line_points = to_js(face2_1line_points)
+
+            face2_1line_geom = THREE.BufferGeometry.new()
+            face2_1line_geom.setFromPoints(face2_1line_points)
+            face2_1line_mesh = THREE.Line.new( face2_1line_geom, line_material2)
+            scene.add(face2_1line_mesh)
+
+            geometries.append(face2_1line_mesh)
+
+            face2_1_1 = THREE.LineCurve.new(face2_1_start1, face2_2_start2 )
+            face2_1mid = THREE.Vector3.new()
+            face2_1_1.getPoint(division, face2_1mid)
+            face2_1_2 = THREE.LineCurve.new(face2_2_start2,face2_3_start3 )
+            face2_1_2mid = THREE.Vector3.new()
+            face2_1_2.getPoint(division,face2_1_2mid)
+            face2_1_3 = THREE.LineCurve.new(face2_3_start3,face2_1_start1 )
+            face2_1_3mid = THREE.Vector3.new()
+            face2_1_3.getPoint(division, face2_1_3mid)
+
+            
+            face2_1_start1 = face2_1mid
+            face2_2_start2 = face2_1_2mid
+            face2_3_start3 = face2_1_3mid
+
+
+            #face3---------------------------------------------------------------------------------------------------------------------------
+
+            face3_1line_points = [face3_1_start1, face3_2_start2, face3_3_start3, face3_1_start1]
+            face3_1line_points = to_js(face3_1line_points)
+
+            face3_1line_geom = THREE.BufferGeometry.new()
+            face3_1line_geom.setFromPoints(face3_1line_points)
+            face3_1line_mesh = THREE.Line.new( face3_1line_geom, line_material2)
+            scene.add(face3_1line_mesh)
+
+            geometries.append(face3_1line_mesh)
+
+            face3_1_1 = THREE.LineCurve.new(face3_1_start1, face3_2_start2 )
+            face3_1mid = THREE.Vector3.new()
+            face3_1_1.getPoint(division, face3_1mid)
+            face3_1_2 = THREE.LineCurve.new(face3_2_start2,face3_3_start3 )
+            face3_1_2mid = THREE.Vector3.new()
+            face3_1_2.getPoint(division,face3_1_2mid)
+            face3_1_3 = THREE.LineCurve.new(face3_3_start3,face3_1_start1 )
+            face3_1_3mid = THREE.Vector3.new()
+            face3_1_3.getPoint(division, face3_1_3mid)
+
+            
+            face3_1_start1 = face3_1mid
+            face3_2_start2 = face3_1_2mid
+            face3_3_start3 = face3_1_3mid   
+
+            #face4----------------------------------------------------------------------------------------------------------------------------
+
+            face4_1line_points = [face4_1_start1, face4_2_start2, face4_3_start3, face4_1_start1]
+            face4_1line_points = to_js(face4_1line_points)
+
+            face4_1line_geom = THREE.BufferGeometry.new()
+            face4_1line_geom.setFromPoints(face4_1line_points)
+            face4_1line_mesh = THREE.Line.new( face4_1line_geom, line_material2)
+            scene.add(face4_1line_mesh)
+
+            geometries.append(face4_1line_mesh)
+
+
+            face4_1_1 = THREE.LineCurve.new(face4_1_start1, face4_2_start2 )
+            face4_1mid = THREE.Vector3.new()
+            face4_1_1.getPoint(division, face4_1mid)
+            face4_1_2 = THREE.LineCurve.new(face4_2_start2,face4_3_start3 )
+            face4_1_2mid = THREE.Vector3.new()
+            face4_1_2.getPoint(division,face4_1_2mid)
+            face4_1_3 = THREE.LineCurve.new(face4_3_start3,face4_1_start1 )
+            face4_1_3mid = THREE.Vector3.new()
+            face4_1_3.getPoint(division, face4_1_3mid)
+
+            
+            face4_1_start1 = face4_1mid
+            face4_2_start2 = face4_1_2mid
+            face4_3_start3 = face4_1_3mid   
+
+
+            #geometries.position.set(THREE.Vector3.new(0,0,1))
+
+            for geometry in geometries:
+                
+                geometry2 = geometry.clone().translateZ(1)
+                scene.add(geometry2)
+                console.log(geometry2)
+
+                geometry3 = geometry.clone().translateZ(-1)
+                scene.add(geometry3)
+   
+       
 
 # Simple render and animate
 def render(*args):
     window.requestAnimationFrame(create_proxy(render))
-    update_system()
     controls.update()
     composer.render()
+    update()
+
+def update():
+
+    global max_iterations, scene, division
+
+    if max_iterations != geom_params.x or division != geom_params.division:
+    
+       scene.clear()
+       max_iterations = geom_params.x
+       division = geom_params.division
+
+       my_element_system = system(0,max_iterations,"X")
+       draw_system(my_element_system)
+       
 
 # Graphical post-processing
 def post_process():
@@ -218,15 +398,14 @@ def post_process():
 
     pixelRatio = window.devicePixelRatio
 
-    fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * pixelRatio )
-    fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * pixelRatio )
+    fxaa_pass.material.uniforms.resolution.value.x = 1 / ( window.innerWidth * 5000*pixelRatio )
+    fxaa_pass.material.uniforms.resolution.value.y = 1 / ( window.innerHeight * 4000* pixelRatio )
    
     global composer
     composer = THREE.EffectComposer.new(renderer)
     composer.addPass(render_pass)
     composer.addPass(fxaa_pass)
 
-# Adjust display when window size changes
 def on_window_resize(event):
 
     event.preventDefault()
@@ -241,7 +420,7 @@ def on_window_resize(event):
 
     #post processing after resize
     post_process()
-#-----------------------------------------------------------------------
-#RUN THE MAIN PROGRAM
+
+    #RUN THE MAIN PROGRAM
 if __name__=='__main__':
     main()
